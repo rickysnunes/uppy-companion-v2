eb6286c70c43adfa6e2839c606cde11a2c2af92c
rickysnunes <ricardosnunes@outlook.pt>
2026-01-15 23:26:51 +0000

    Create index.js

---
+++ index.js
@@ -0,0 +1,25 @@
+import express from 'express'
+import companion from '@uppy/companion'
+
+const app = express()
+
+app.use(
+  companion.app({
+    providerOptions: {
+      google: {
+        key: process.env.GOOGLE_CLIENT_ID,
+        secret: process.env.GOOGLE_CLIENT_SECRET
+      }
+    },
+    server: {
+      host: process.env.HOST,
+      protocol: 'https'
+    },
+    filePath: './uploads',
+    secret: process.env.COMPANION_SECRET
+  })
+)
+
+app.listen(process.env.PORT || 3020, () => {
+  console.log('Uppy Companion running')
+})
