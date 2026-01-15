f2b02bffa6a60dc3a694f0ba57535ab041a6dbe8
rickysnunes <ricardosnunes@outlook.pt>
2026-01-15 23:59:32 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,28 @@
 import express from 'express'
-import { app as companionApp } from '@uppy/companion'
+import Companion from '@uppy/companion'
 
 const app = express()
 
 // Configura o Companion
-app.use(
-  companionApp({
-    providerOptions: {
-      drive: {
-        key: process.env.GOOGLE_CLIENT_ID,
-        secret: process.env.GOOGLE_CLIENT_SECRET
-      }
-    },
-    server: {
-      host: process.env.HOST || 'localhost:3020',
-      protocol: 'https'
-    },
-    uploadUrls: ['*'], // ok para testar
-    filePath: '/tmp', // Railway não permite escrever em ./uploads
-    secret: process.env.COMPANION_SECRET || 'uma-senha-secreta'
-  })
-)
+const companionOptions = {
+  providerOptions: {
+    drive: {
+      key: process.env.GOOGLE_CLIENT_ID,
+      secret: process.env.GOOGLE_CLIENT_SECRET
+    }
+  },
+  server: {
+    host: process.env.HOST,
+    protocol: 'https'
+  },
+  uploadUrls: ['*'], // só para testar
+  filePath: '/tmp', // Railway só permite escrever em /tmp
+  secret: process.env.COMPANION_SECRET
+}
+
+// O Companion retorna um app válido para usar como middleware
+const companionApp = Companion.app(companionOptions)
+app.use(companionApp)
 
 const port = process.env.PORT || 3020
 app.listen(port, () => {
